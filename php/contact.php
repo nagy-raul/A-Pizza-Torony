83e32ab11a4650c91c679be2975a0b0eda8ac813
nagy-raul <nagy.raul-2020@keri.mako.hu>
2025-02-11 09:42:20 +0100

    kapcsolat

--- php/contact.php
+++ php/contact.php
@@ -11,7 +11,7 @@ $args = Util::getArgs();
 $db = new Database();
 
 // Set SQL command
-$query = "INSERT INTO `kapcsolat`(`nev`, `email`, `telszam`, `targy`, `uzenet`) VALUES (:name, :email, :countryCode, :phone, :targy, :uzenet)";
+$query = "INSERT INTO `kapcsolat`(`nev`, `email`, `orszagkod`, `telszam`, `targy`, `uzenet`) VALUES (:name, :email, :countryCode, :phone, :targy, :uzenet)";
 
 // Execute SQL command
 $result = $db->execute($query, $args);
