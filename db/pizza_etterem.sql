ec1a4b7d34841b33909984eadc4ea9caed487902
nagy-raul <nagy.raul-2020@keri.mako.hu>
2025-04-08 10:05:47 +0200

    database

--- db/pizza_etterem.sql
+++ db/pizza_etterem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Ápr 08. 07:43
+-- Létrehozás ideje: 2025. Ápr 08. 09:44
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.1.17
 
